5371cb6f032c91ebadb0919ee1996e3d43f82d10
iamakhildabral <akhildabral02@gmail.com>
2023-07-15 14:05:30 +0530

    checking for substring in C#

--- Program.cs
+++ Program.cs
@@ -8,7 +8,18 @@ namespace Test
         static void Main(string[] args)
         {
             //Palindrome.PalindromeUtils.IsPalindrome();
-            Armstrong.Armstrong.isArmstrong();
+            //Armstrong.Armstrong.isArmstrong();
+            string str  = "";
+            str = Console.ReadLine();
+            System.Console.WriteLine("Enter the substring");
+            if(str.Contains(Console.ReadLine())){
+                System.Console.WriteLine("substring exist");
+            }else{
+                System.Console.WriteLine("Not Exist");
+            }
+
+            
+
         }
     }
 }
